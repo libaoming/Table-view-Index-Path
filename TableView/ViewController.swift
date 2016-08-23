55086356891998e7190c9e1c26a5411af3cd0e49
master <libaoming2@gmail.com>
2016-08-23 13:46:53 +0800

    complete tableview indexpath in use

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var cellContent = [Int]()
+    
+    func cellContentInit(){
+     
+        var i = 1
+        while i <= 15 {
+            cellContent.append(i)
+            i += 1
+        }
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
+    
+    
+    
+   
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+       
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = String(indexPath.row + 1 )
+        
+        return cell
+    }
+    
+
 
 
 }
